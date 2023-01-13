ffeaa675dbed99642d9298768866d6d5f31394ac
diegoinjp <diego.vpda@gmail.com>
2023-01-13 12:18:36 +0900

    add menu and icons

--- src/components/MenuLinks.tsx
+++ src/components/MenuLinks.tsx
@@ -1,13 +1,15 @@
-import Modal from './Modal'
-
 function MenuLinks({ itemList }: { itemList: string[] }) {
   return (
-    <div className="flex flex-col gap-10 text-2xl text-left">
-      {itemList.map((item, index) => (
-        <button onClick={() => console.log(index)}>{item}</button>
-      ))}
-      <Modal>{itemList}</Modal>
-    </div>
+    <>
+      <div className="flex flex-col gap-10 text-2xl">
+        {itemList.map((item, index) => (
+          <p onClick={() => console.log(index)} class="relative group hover:text-hotpink-main text-end cursor-pointer">
+            {item}
+            <span className="absolute -bottom-2 right-0 transition-all duration-200 h-1 w-0 group-hover:bg-hotpink-main group-hover:w-full"></span>
+          </p>
+        ))}
+      </div>
+    </>
   )
 }
 
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,7 +6,7 @@ export interface ModalProps {
 }
 
 const Modal = ({ id, title }: ModalProps) => {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
   const closeModal = () => {
     setOpen(false)
     console.log('closing modal')
@@ -15,8 +15,10 @@ const Modal = ({ id, title }: ModalProps) => {
     <>
       {open && (
         <div class="fixed w-full h-screen bg-black/40 grid place-items-center" onClick={closeModal}>
-          <div class="bg-hotpink-white p-8 w-[30rem] text-gray-700 text-left flex flex-col gap-3">
+          <div class="bg-hotpink-white p-8 w-[30rem] text-gray-700 text-left flex flex-col gap-3 relative" onClick={(e) => e.stopPropagation()}>
             <h1 class="text-4xl uppercase font-semibold first-letter:text-hotpink-main">About</h1>
+            <span class="absolute w-5/12 h-1 top-10 right-8 bg-hotpink-main"></span>
+            <span class="absolute w-6/12 h-1 top-14 right-8 bg-gray-700"></span>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, adipisci iure tempora odit recusandae exercitationem, veniam ducimus soluta explicabo dolor est laborum, doloremque culpa obcaecati. In voluptatibus doloremque odio temporibus.</p>
           </div>
         </div>
