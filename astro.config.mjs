00c1c9500bb788972afd7d4f6bfaf0a760fb83fd
diegoinjp <diego.vpda@gmail.com>
2023-01-15 18:30:12 +0900

    Revert "Add integratopms"
    This reverts commit e6e0492bf7b117b70b09e0f5aeff064eaa898284.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,6 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
-import compress from "astro-compress";
-
-// https://astro.build/config
-import robotsTxt from "astro-robots-txt";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://regainstm.netlify.app/',
-  integrations: [tailwind(), preact(), prefetch(), compress(), robotsTxt()]
+  integrations: [tailwind(), preact()]
 });
